b377ba89bc1b112b4e464c087aa9289826b79c21
Viljami Uusihärkälä <viljami.uusiharkala@aalto.fi>
2019-08-15 12:48:18 +0300

    commit build versions of front

---
+++ build/precache-manifest.eb7a633466eb3da4b2fc7da58a0b0b64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e98ba68cc281c689f0f20fc9512d8e72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0dd30bb1c8049f1c7c0",
+    "url": "/static/css/main.a78e38c0.chunk.css"
+  },
+  {
+    "revision": "6a19a0beea039a280f26",
+    "url": "/static/js/2.3c62b441.chunk.js"
+  },
+  {
+    "revision": "a0dd30bb1c8049f1c7c0",
+    "url": "/static/js/main.ce74f997.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- index.js
+++ index.js
@@ -10,6 +10,7 @@ morgan.token('body', (req) => {
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
+app.use(express.static('build'))
 
 let persons = [
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
